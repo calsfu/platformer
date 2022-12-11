b374b732d50654d75cbfdbd608277d201b1dbaf6
Yohan Kim <yohankim03@gmail.com>
2022-12-11 18:08:31 -0500

    added ice tile

--- Platformer2/Assets/PlayerMovement.cs
+++ Platformer2/Assets/PlayerMovement.cs
@@ -5,19 +5,24 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float walkSpeed = 3.5f;
-    public float jumpBoost = 2f;
+    public float jumpBoost = 1.5f;
     public float jumpSpeed = 0.0f;
     private bool canJump = true;
     private float direction = 0;
 
     public Transform groundCheck;
     public float groundCheckRadius;
+    
     public LayerMask groundLayer;
+    public LayerMask iceGroundLayer;
     public bool isTouchingGround;
+    public bool isTouchingIceGround;
 
-    public PhysicsMaterial2D bounceMaterial, normalMaterial;
+    public PhysicsMaterial2D bounceMaterial, normalMaterial, iceMaterial;
 
     private Rigidbody2D player;
+
+    public bool isMoving;
 
 
     // Start is called before the first frame update
@@ -33,14 +38,14 @@ public class PlayerMovement : MonoBehaviour
         direction = Input.GetAxis("Horizontal");
         player.velocity = new Vector2(player.velocity.x, player.velocity.y);
         isTouchingGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        isTouchingIceGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, iceGroundLayer);
 
         //Makes the player face the correct direction and move left or right when jumping
         if (direction > 0f)
         {
-            
-            if (!isTouchingGround)
+            if (!isTouchingGround && !isTouchingIceGround)
             {
-                if (jumpSpeed == 0.0f && isTouchingGround)
+                if (jumpSpeed == 0.0f && (isTouchingGround || isTouchingIceGround))
                     player.velocity = new Vector2(direction * walkSpeed, player.velocity.y);
 
             }
@@ -50,10 +55,9 @@ public class PlayerMovement : MonoBehaviour
         }
         else if (direction < 0f)
         {
-            transform.localScale = new Vector2(-1, 1);
-            if (!isTouchingGround)
+            if (!isTouchingGround && !isTouchingIceGround)
             {
-                if (jumpSpeed == 0.0f && isTouchingGround)
+                if (jumpSpeed == 0.0f && (isTouchingGround || isTouchingIceGround))
                     player.velocity = new Vector2(direction * walkSpeed, player.velocity.y);
             }
             else
@@ -70,30 +74,35 @@ public class PlayerMovement : MonoBehaviour
         {
             player.sharedMaterial = normalMaterial;
         }
+
         //Jump
-        if (Input.GetKey("space") && isTouchingGround && canJump)
+        Debug.Log(player.velocity.magnitude);
+        if (player.velocity.magnitude == 0)
         {
-            jumpSpeed += 0.01f;
-        }
-        if (Input.GetKeyDown("space") && isTouchingGround && canJump)
-        {
-            player.velocity = new Vector2(0.0f, player.velocity.y);
-        }
-        if (jumpSpeed >= 10f && isTouchingGround)
-        {
-            float tempx = direction * walkSpeed* jumpBoost;
-            float tempy = jumpSpeed;
-            player.velocity = new Vector2(tempx, tempy);
-            Invoke("ResetJump", 1f);
-        }
-        if (Input.GetKeyUp("space"))
-        {
-            if (isTouchingGround)
+            if (Input.GetKey("space") && (isTouchingGround || isTouchingIceGround) && canJump)
             {
-                player.velocity = new Vector2(direction * walkSpeed, jumpSpeed * jumpBoost);
-                jumpSpeed = 0f;
+                jumpSpeed += 0.01f;
             }
-            canJump = true;
+            if (Input.GetKeyDown("space") && (isTouchingGround || isTouchingIceGround) && canJump)
+            {
+                player.velocity = new Vector2(0.0f, player.velocity.y);
+            }
+            if (jumpSpeed >= 10f && (isTouchingGround || isTouchingIceGround))
+            {
+                float tempx = direction * walkSpeed * jumpBoost;
+                float tempy = jumpSpeed;
+                player.velocity = new Vector2(tempx, tempy);
+                Invoke("ResetJump", 1f);
+            }
+            if (Input.GetKeyUp("space"))
+            {
+                if (isTouchingGround || isTouchingIceGround)
+                {
+                    player.velocity = new Vector2(direction * walkSpeed, jumpSpeed * jumpBoost);
+                    jumpSpeed = 0f;
+                }
+                canJump = true;
+            }
         }
     }
     void ResetJump()
@@ -101,5 +110,6 @@ public class PlayerMovement : MonoBehaviour
         canJump = false;
         jumpSpeed = 0;
     }
+
 }
 
